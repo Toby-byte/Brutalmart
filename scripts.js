1665851b40940abdd0b89e5553899970e3522ece
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-04-12 15:22:32 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function searchProducts() {
+    var input = document.querySelector('input[type="text"]').value;
+    alert("Search for " + input);
+    // Implement actual search functionality or API calls here
+}
